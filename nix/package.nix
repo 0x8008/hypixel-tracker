7dc7fb4e820114bc7a85323fac0d986109696e74
0x8008 <balu@xdapierdolnik.pl>
2023-01-23 12:53:43 +0100

    bump

--- nix/package.nix
+++ nix/package.nix
@@ -9,7 +9,7 @@
   }:
     buildPythonPackage {
       pname = "hypixel-tracker";
-      version = "0.0.1";
+      version = "0.0.2";
       src = ../.;
       propagatedBuildInputs = [requests];
       doCheck = false;
--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from distutils.core import setup
 
 setup(
     name='hypixel-tracker',
-    version='0.0.1',
+    version='0.0.2',
     scripts=['main.py',],
 )
